9086edffd93d34e639197482806c73e2ba41157b
salmaUsamaa <105728467+salmaUsamaa@users.noreply.github.com>
2024-10-25 17:07:41 +0300

    phase0

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/register">Register</Link> | <Link to="/login">Login</Link>
+        </nav>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
